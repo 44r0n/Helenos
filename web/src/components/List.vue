3d6568b550aa0ea2bfebd81e092c04d65f7ec093
Aarón <aaron.sanchez.1988@icloud.com>
2019-07-07 20:13:38 +0200

    Add ItemComponent

--- web/src/components/List.vue
+++ web/src/components/List.vue
@@ -11,13 +11,18 @@
                 :todo="item"
                 :key="item.id"></todo-item>
         </ol>
+        <AddItemComponent @addedItem="addItem"></AddItemComponent>
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import TodoItem from "./TodoItem.vue";
+import AddItemComponent from "./AddItemComponent.vue";
+
 Vue.component('todo-item', TodoItem);
+Vue.component('AddItemComponent', AddItemComponent);
+
 @Component
 export default class List extends Vue {
     todos = [
@@ -31,7 +36,13 @@ export default class List extends Vue {
         { id: 0, text: 'Vegetables' },
         { id: 1, text: 'Cheese' },
         { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
+    ];    
+
+    addItem(args: any) {
+        let newItem = args as {id: number, text: string};
+        newItem.id = this.groceryList.length;
+        this.groceryList.push(newItem);
+    }
 }
 </script>
 
